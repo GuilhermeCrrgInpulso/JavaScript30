33ee3eff4151a262d0d28f34a735b3f510b48208
GuilhermeCrrgInpulso <guilherme.carrega@inpulso.com.br>
2025-02-14 09:56:48 -0300

    11

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -2,15 +2,35 @@ const playButton = document.querySelector('.toggle');
 const video = document.querySelector('video');
 const volume = document.getElementsByName('volume')[0];
 const speed = document.getElementsByName('playbackRate')[0];
-const skipTimeBts = document.querySelectorAll("[data-skip]");
-let playing = false
+const skipTimeBts = document.querySelectorAll('[data-skip]');
+const progressBar = document.querySelector('.progress__filled');
+const progress = document.querySelector('.progress');
 
+let playing = false;
+let mouseClick = false;
 
-playButton.addEventListener("click", playVideo);
+progress.addEventListener('mousedown',() => { mouseClick = true});
+progress.addEventListener('mouseup',() => { mouseClick = false});
+progress.addEventListener('click',progressByClick);
+progress.addEventListener('mousemove',progressByClick);
+
+video.addEventListener('click', playVideo);
+playButton.addEventListener('click', playVideo);
 volume.addEventListener('input', changeVolume);
 speed.addEventListener('input', changeSpeed);
-skipTimeBts.forEach((skipButton) => skipButton.addEventListener('click',skipTime))
+video.addEventListener('timeupdate', progressChange);
+skipTimeBts.forEach((skipButton) => skipButton.addEventListener('click',skipTime));
 
+function progressByClick (event) {
+    if (mouseClick || event.type == 'click') {
+        let time = (event.offsetX / progress.offsetWidth) * video.duration;
+        video.currentTime = time;
+    }
+}
+
+function progressChange () {
+    progressBar.style.flexBasis = `${(video.currentTime / video.duration) * 100}%`;
+}
 
 function skipTime () {
     video.currentTime += parseInt(this.dataset.skip);
@@ -27,9 +47,11 @@ function changeVolume () {
 function playVideo (){
     if(!playing){
         video.play();
+        playButton.textContent = '❚ ❚';
         playing = true;
     } else{
         video.pause();
+        playButton.textContent = '►';
         playing = false;
     }
 }
